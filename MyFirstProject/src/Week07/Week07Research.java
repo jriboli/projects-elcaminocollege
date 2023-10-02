0cf0ac8d0100f41ff3e10d73ffee6ba705b9e46a
jriboli <jmriboli@yahoo.com>
2023-10-02 12:10:01 -0700

    more Week07 stuff

--- MyFirstProject/src/Week07/Week07Research.java
+++ MyFirstProject/src/Week07/Week07Research.java
@@ -2,16 +2,79 @@ package Week07;
 
 public class Week07Research {
 	//1. What is a database?  What is a schema?  Compare and contrast?
+	
+	/*
+	 * ANSWER: (https://stackoverflow.com/questions/5323740/difference-between-database-and-schema)
+	 * 	A database is like a filing cabinet, it holds multiple things. Such as data, log file, and x numbers of schemas with
+	 * it. It is a unit on its own. Schemas though are like the folders within a file cabinet. And are used to group 
+	 * objects/tables together. This leads to permission being controlled at the schema level. 
+	 */
 
 	//2. What is a DBMS?  What is an RDBMS?  Compare and contrast.
+	
+	/*
+	 * ANSWER: (https://www.geeksforgeeks.org/difference-between-rdbms-and-dbms/)
+	 * 	DBMS is Database Management System, in short it is software used to define, create and maintain a database 
+	 * and provides controlled access to the data. 
+	 * 
+	 * RDBMS is just Relational Database Management System. So it is a type of DBMS, specifically for dealing with 
+	 * relations as well as version key constraints. 
+	 */
 
 	//3. What is SQL?  How is it used?  
+	
+	/*
+	 * 	SQL stands for Structure Query Language. It is used to retrieve data from a database. The data in a database is
+	 * stored across several files and in a complex structure. SQL allows us to write simple human readable queries to 
+	 * retrieve data. 
+	 */
 
 	//4. Our course uses MySQL.  What is the difference between MySQL and SQL?
+	
+	/* 
+	 * ANSWER: (https://www.coursera.org/articles/sql-vs-mysql)
+	 * 	MySQL vs SQL, MySQL is a specific type of SQL. There are several options of databases that use SQL. Microsoft SQL being
+	 * another one. This simply means that the way we access the information stored in the database is by using queries,
+	 * based on a standard syntax. 
+	 */
 
 	//5. Describe MySQL?  What type of database management system is it?  What company owns it?  What makes it unique?
+	
+	/*
+	 * MySQL is a relational database system. It uses the SQL language for querying. It is owned by the company Oracle. 
+	 * It is typically used in web applications, and it has a free open source version that folks can use - Community Edition.
+	 */
 
 	//6. How is data organized in an RDBMS?   What are the constructs that allow users to easily organize and access data? 
+	
+	/*
+	 * ANSWER: (https://www.geeksforgeeks.org/rdbms-architecture/)
+	 *	Is a RDBMS database, the data is stored in the form of tables, also called relations. These tables are divided into 
+	 * rows, also called records and columns (fields). Some advantages are it facilitates primary keys, which help in unique
+	 * identification of rows. It facilitates a common column to be shared amongst tables. Helps in unique id'ing of a row.
+	 * Additionally, RDBMS implements SQL for use when accessing the database data.  
+	 */
 
 	//7. What are ten different data types MySQL provides?
+	
+	/*
+	 * ANSWER: (https://www.w3schools.com/mysql/mysql_datatypes.asp)
+	 * 	Here are some of the data types MYSQL provides:
+	 * - INTEGER
+	 * - SMALLINT
+	 * - BIGINT
+	 * - NUMERIC
+	 * - DECIMAL
+	 * - REAL
+	 * - DOUBLE PERCISION
+	 * - FLOAT
+	 * - CHAR
+	 * - VARCHAR
+	 * - BIT
+	 * - BLOB
+	 * - DATE
+	 * - TIME
+	 * - TIMESTAMP
+	 */
+	
 }
