a0ee465fd9c9f9ce4da867cb38affd236aa0127f
jriboli <jmriboli@yahoo.com>
2023-10-02 09:20:13 -0700

    Starting Week07

---
+++ MyFirstProject/src/Week07/Week07Research.java
@@ -0,0 +1,17 @@
+package Week07;
+
+public class Week07Research {
+	//1. What is a database?  What is a schema?  Compare and contrast?
+
+	//2. What is a DBMS?  What is an RDBMS?  Compare and contrast.
+
+	//3. What is SQL?  How is it used?  
+
+	//4. Our course uses MySQL.  What is the difference between MySQL and SQL?
+
+	//5. Describe MySQL?  What type of database management system is it?  What company owns it?  What makes it unique?
+
+	//6. How is data organized in an RDBMS?   What are the constructs that allow users to easily organize and access data? 
+
+	//7. What are ten different data types MySQL provides?
+}
