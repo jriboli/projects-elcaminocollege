9ff5cb665839b282326869557ee20651cd036495
jriboli <jmriboli@yahoo.com>
2023-10-13 10:32:40 -0700

    Begin Week08

--- MyFirstProject/src/Week05/Week05OOP/Week05OOPLab.java
+++ MyFirstProject/src/Week05/Week05OOP/Week05OOPLab.java
@@ -17,88 +17,88 @@ public class Week05OOPLab {
 	public static void main(String[] args) {
 	
 		
-		// A standard deck of playing cards has 52 cards as specified below 
-		//		i. There are 4 suits:  Clubs, Diamonds, Hearts, & Spades
-		//				
-		//	   ii. Each suit has 13 cards:  Two, Three, Four, Five, Six, Seven, 
-		//									 Eight, Nine, Ten, Jack, Queen, King & Ace
-		//
-		//	  iii. Comparing Cards:  When comparing two cards, Ace is high and Two is low.
-		//							 to make this easy, a Two will have a value of 2, a
-		//							 Three will have a value of 3, ... and an Ace will have
-		//							 a value of 14.
-		//
-	
-		
-		// 1. Card Class:
-		//		Create a class called Card to represent a standard playing card. 
-		//		Fields:   The Card class should have the following fields:
-		// 			a. name field
-		//			b. suit field
-		//			c. value field for comparing against other cards.
-		//
-		//		Methods:  This class can have any useful method.
-		//			a. describe() to display the card information to the Console.
-		//			b. Getters & Setters 
-		//
-		System.out.println("\nQuestion 1: Card Class");
-		// Add your code here to instantiate a Card
-		Card card = new Card("spades", "ace");
-		
-		
-		// Call the describe method on the newly instantiated card.
-		card.describe();
-		
-		
-		
-		
-
-		// 2. Deck Class:
-		//		Create a class called Deck.
-		//		Fields:  This class should have a list of Card field called cards 
-		//				 that will hold all the cards in the deck. 
-		//			List<Card> cards = new ArrayList<Card>(); 
-		//
-		//		Constructor: The constructor for the Deck Class should
-		// 			instantiate all 52 standard playing cards and add them to the cards list.
-		//
-		//		Methods:  
-		//			a.  describe() to describe the deck to the Console -- 
-		//					print out all of the cards in the deck.
-		//
-		System.out.println("\nQuestion 2: Deck Class");
-	    // Add your code here to instantiate a Deck	
-	    Deck deck = new Deck();
-	    
-	    // Call the describe method on the newly instantiated deck.
-	    deck.describe();
-	    
-	    
-	    
-		
-		
-		// 3. Deck shuffle() Method:
-		//		Add a shuffle method within the Deck Class
-		System.out.println("\nQuestion 3: Deck shuffle() method");
-		// Test your method here
-		deck.shuffle(7);
-		
-		
-		// Call the describe method on the newly shuffled deck.
-		deck.describe();
-		
-		
-		
-		// 4. Deck draw() Method:
-		//		Add a draw method within the Deck Class
-		System.out.println("\nQuestion 4: Deck draw() method");
-		// Test your method here
-		Card drawnCard = deck.draw();
-		
-		drawnCard.describe();
-		System.out.println("-----------------------------------------------------");
-		deck.describe();
-		
+//		// A standard deck of playing cards has 52 cards as specified below 
+//		//		i. There are 4 suits:  Clubs, Diamonds, Hearts, & Spades
+//		//				
+//		//	   ii. Each suit has 13 cards:  Two, Three, Four, Five, Six, Seven, 
+//		//									 Eight, Nine, Ten, Jack, Queen, King & Ace
+//		//
+//		//	  iii. Comparing Cards:  When comparing two cards, Ace is high and Two is low.
+//		//							 to make this easy, a Two will have a value of 2, a
+//		//							 Three will have a value of 3, ... and an Ace will have
+//		//							 a value of 14.
+//		//
+//	
+//		
+//		// 1. Card Class:
+//		//		Create a class called Card to represent a standard playing card. 
+//		//		Fields:   The Card class should have the following fields:
+//		// 			a. name field
+//		//			b. suit field
+//		//			c. value field for comparing against other cards.
+//		//
+//		//		Methods:  This class can have any useful method.
+//		//			a. describe() to display the card information to the Console.
+//		//			b. Getters & Setters 
+//		//
+//		System.out.println("\nQuestion 1: Card Class");
+//		// Add your code here to instantiate a Card
+//		Card card = new Card("spades", "ace");
+//		
+//		
+//		// Call the describe method on the newly instantiated card.
+//		card.describe();
+//		
+//		
+//		
+//		
+//
+//		// 2. Deck Class:
+//		//		Create a class called Deck.
+//		//		Fields:  This class should have a list of Card field called cards 
+//		//				 that will hold all the cards in the deck. 
+//		//			List<Card> cards = new ArrayList<Card>(); 
+//		//
+//		//		Constructor: The constructor for the Deck Class should
+//		// 			instantiate all 52 standard playing cards and add them to the cards list.
+//		//
+//		//		Methods:  
+//		//			a.  describe() to describe the deck to the Console -- 
+//		//					print out all of the cards in the deck.
+//		//
+//		System.out.println("\nQuestion 2: Deck Class");
+//	    // Add your code here to instantiate a Deck	
+//	    Deck deck = new Deck();
+//	    
+//	    // Call the describe method on the newly instantiated deck.
+//	    deck.describe();
+//	    
+//	    
+//	    
+//		
+//		
+//		// 3. Deck shuffle() Method:
+//		//		Add a shuffle method within the Deck Class
+//		System.out.println("\nQuestion 3: Deck shuffle() method");
+//		// Test your method here
+//		deck.shuffle(7);
+//		
+//		
+//		// Call the describe method on the newly shuffled deck.
+//		deck.describe();
+//		
+//		
+//		
+//		// 4. Deck draw() Method:
+//		//		Add a draw method within the Deck Class
+//		System.out.println("\nQuestion 4: Deck draw() method");
+//		// Test your method here
+//		Card drawnCard = deck.draw();
+//		
+//		drawnCard.describe();
+//		System.out.println("-----------------------------------------------------");
+//		deck.describe();
+//		
 		
 		
 		
@@ -115,42 +115,89 @@ public class Week05OOPLab {
 		// 			and deal the cards out to the "players" in the Map.
 		System.out.println("\nQuestion 5: Create Game");
 		// Call your method here
-		Map<String, List<Card>> theGame = deal(2);
-		List<Card> playerOneHand = theGame.get("Player 1");
-		List<Card> playerTwoHand = theGame.get("Player 2");
+		int numberOfPlayers = 5;
+		Map<String, List<Card>> theGame = deal(numberOfPlayers);
 		
-		System.out.println("-----------------------------------------------------");
-		System.out.println("Player Ones Hand: ");
-		for(Card c : playerOneHand) {
-			c.describe();
-		}
-		
-		System.out.println("-----------------------------------------------------");
-		System.out.println("Player Twos Hand: ");
-		for(Card c : playerTwoHand) {
-			c.describe();
+		for(int i = 1; i <= numberOfPlayers; i++) {
+			List<Card> playerHand = theGame.get("Player " + i);
+			
+			System.out.println("-----------------------------------------------------");
+			System.out.println("Player " + i + " Hand: ");
+			System.out.println("Card count = " + playerHand.size());
+			for(Card c : playerHand) {
+				c.describe();
+			}
 		}		
 		
 	} 
 	
 	// Method 5:
 	public static Map<String, List<Card>> deal(int numberOfPlayers) {
-		Map<String, List<Card>> playerHands = new HashMap<String, List<Card>>();
+		Map<String, List<Card>> gameBoard = new HashMap<String, List<Card>>();
+		//Map<String, Integer> testMap = new HashMap<String, String>();
+		
+		// Create a fresh Deck
 		Deck deck = new Deck();
 		deck.shuffle(7);
 		
-		int numberOfCardsPerPlayer = 5;
+		// Find number of cards to deal
+		int cardsRemainingAfterDeal = 52 % numberOfPlayers;
+		int numberOfCardsPerPlayer = (52 - cardsRemainingAfterDeal)/numberOfPlayers;
+		System.out.println("Number of Cards per Player - " + numberOfCardsPerPlayer);
+		
+		// Loop through creating the player hands
+//		for(int i = 1; i <= numberOfPlayers; i++) {
+//			// Create player hand
+//			List<Card> playerHand = new ArrayList<>();
+//			
+//			// Loop through populating the player hand with X amount of cards
+//			for(int j = 0; j < numberOfCardsPerPlayer; j++) {
+//				playerHand.add(deck.draw());
+//			}		
+//			
+//			// Add the hand to gameBoard
+//			gameBoard.put("Player " + i, playerHand);
+//		}
+		
+		// Loop through more realistically - because Pamela
 		for(int i = 1; i <= numberOfPlayers; i++) {
-			List<Card> playerCards = new ArrayList<>();
-			for(int j = 0; j < numberOfCardsPerPlayer; j++) {
-				playerCards.add(deck.draw());
-			}
+			// Create player hand
+			List<Card> playerHand = new ArrayList<>();		
 			
-			playerHands.put("Player " + i, playerCards);
+			// Add the hand to gameBoard
+			gameBoard.put("Player " + i, playerHand);
+			//gameBoard.put("player " + 1, null);
 		}
 		
+		for(int j = 52; j > 0; j--) {
+			// 
+			if(j > cardsRemainingAfterDeal) {
+				int whoToDealTo = j % numberOfPlayers + 1;
+				// playerName = "Player " + whoToDealTo;
+				//System.out.println("Player Name : " + playerName);
+				
+				//List<Card> playerHand = gameBoard.get(playerName);
+				//playerHand.add(deck.draw());
+				
+				//System.out.println(playerHand);
+				gameBoard.get("Player " + whoToDealTo).add(deck.draw());
+				
+			} else {
+				// Do something
+				System.out.println("Skipped not enough cards -- " + j);
+			}
+		}
 		
-		return playerHands;
+		double numberOfHandOutLoops = Math.floor(52/5);
+		System.out.println("Here we go --- " + numberOfHandOutLoops);
+		
+//		for (int k = 52; k > 0; k -= numberOfPlayers) {
+//			
+//			if(k - numberOfPlayers)
+//		}
+		
+		
+		return gameBoard;
 	}
 
 }
---
+++ MyFirstProject/src/Week08/Week08Research.java
@@ -0,0 +1,50 @@
+package Week08;
+
+public class Week08Research {
+//	1. Describe each of the ten (10) MySQL data types.  
+//
+//	(a) How is each data type used?
+//
+//	(b) What makes each unique?
+	
+	/*
+	 * ANSWER:
+	 * 
+	 */
+//
+//
+//	2. Research primary & foreign keys in RDBMSs?  What are they?  Why are they useful?
+	
+	/*
+	 * ANSWER:
+	 * 
+	 */
+//
+//	3. Define DDL and DML.   What do the acronyms mean? Describe each one. Give examples.
+	
+	/*
+	 * ANSWER:
+	 * 
+	 */
+//
+//	4. What are INDEXes in MySQL?  Why are they important?  What benefit do they provide?
+	
+	/*
+	 * ANSWER:
+	 * 
+	 */
+//
+//	5. What is a VIEW in an RDBMS.  What is a view?  Why is it useful?
+	
+	/*
+	 * ANSWER:
+	 * 
+	 */
+//
+//	6. What is a JOIN when talking about databases?
+	
+	/*
+	 * ANSWER:
+	 * 
+	 */
+}
