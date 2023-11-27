8d61e223257de4f09b7333f0c8353c8986fa62e6
jriboli <jmriboli@yahoo.com>
2023-11-27 12:08:20 -0800

    Starting Week 13

---
+++ MyFirstProject/src/Week13/Week13_Research.java
@@ -0,0 +1,38 @@
+package Week13;
+
+public class Week13_Research {
+	/*
+	 * 1. How do web APIs work? Why are they useful?
+	 * 
+	 * ANSWER:
+	 * 
+	 * 2. Look up 3 different public web APIs online and describe 3 endpoints from each.  
+	 * 	In your description include the following:
+	 * (a) The URI (or endpoint)
+	 * (b) All of the HTTP verb methods that can be sent to that endpoint.
+	 * (c) A description of what happens when each verb method is sent.
+	 * 
+	 * ANSWER:
+	 * 
+	 * 3. What does REST stand for?  What is REST? What are some of the key concepts that identify REST?
+	 * 
+	 * ANSWER: 
+	 * 
+	 * 4. What is Maven?  Why do we use Maven? What problems does Maven help solve?
+	 * 
+	 * ANSWER:
+	 * 
+	 * 5. What is Swagger?  Why would you use Swagger?  What can be done with Swagger?
+	 * 
+	 * ANSWER:
+	 * 
+	 * 6. What is ARC?  Why would you use ARC?  What can you do with ARC?
+	 * 
+	 * ANSWER:
+	 * 
+	 * 7. What is Postman?  Why would you use Postman?  What can be done with Postman?
+	 * 
+	 * ANSWER:
+	 */
+
+}
